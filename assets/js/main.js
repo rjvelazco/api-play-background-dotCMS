35386affc6664c783cdda54426f1b1a74c245594
rjvelazco <rjvelazco21@gmail.com>
2021-03-02 12:39:59 -0400

    update - beta version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,226 +1,282 @@
 
-// CONST
-const controls = document.querySelector('#controls'),
-img = document.querySelector('#img'),
-imgContainer = document.querySelector('.image-container'),
-paramSpan = document.querySelector('.params'),
-clip = document.querySelector('#clip'),
-spans = document.querySelectorAll('.input-value'),
-coordinates = document.querySelectorAll('.coordinate')
-undos = document.querySelectorAll('.undo'),
-rotateInput = document.querySelectorAll('#rotate'),
-alert = document.querySelector('#alert');
+// DOM ELEMENTS 
+
+const controls  = document.querySelector('#controls'),
+imgContainer    = document.querySelector('.image-container'),
+img             = document.querySelector('#img'),
+paramsContainer = document.querySelector('.params'),
+focalPoints     = document.querySelector('.focal-point');
+rotateInput     = document.querySelector('#rotate'),
+cropInputX      = document.querySelector('#crop-x'),
+cropInputY      = document.querySelector('#crop-y'),
+coordinates     = document.querySelectorAll('.coordinate'),
+inputValues     = document.querySelectorAll('.input-value'),
+undos           = document.querySelectorAll('.undo'),
+clip            = document.querySelector('#clip'),
+alert           = document.querySelector('#alert'),
+setbtn          = document.querySelector('#set');
 
 
-const point = document.querySelector('.focal-point');
-const url =  'https://demo.dotcms.com/contentAsset/image/17e7dc47-79d0-4525-87a4-40fb3429cbd4/fileAsset';
+// BASE URL
+const baseURL = 'https://demo.dotcms.com/contentAsset/image/17e7dc47-79d0-4525-87a4-40fb3429cbd4/fileAsset';
 
-img.addEventListener('load', ()=>{
-    console.log('Imagen Llamada');
-    point.style.left = '50%';
-    point.style.top = '50%';
-    coordinates[0].innerHTML = `50 <small>X</small>`;
-    coordinates[1].innerHTML = `50 <small>Y</small>`;
-    
-});
 
-// VARIABLES
-let hue = saturation = brightness = width = height = focalX = focalY = 0, cropX = cropY = 250, flip = HBS = filters = rotate = resize = crop = '',
-urlParams = '',
-originalWidth = img.getBoundingClientRect().width,
-originalHeight = img.getBoundingClientRect().height;
+// ORIGINAL SIZE
+const originalW = img.getBoundingClientRect().width,
+originalH       = img.getBoundingClientRect().height;
 
+let imgWidth    = img.getBoundingClientRect().width;
+imgHeight       = img.getBoundingClientRect().height;
+
+// IMAGE CONTAINER SIZE
+const containerW = imgContainer.getBoundingClientRect().width;
+containerH       = imgContainer.getBoundingClientRect().height;
+
+
+// Variables
+let hue = saturation = brightness = 0,
+focalX  = focalY = 0.50,
+cropX   = cropY  = 250,
+width   = height = 0;
+
+// URL PARAMS 
+let HBS = filters = rotate = resize = crop = flip = quality = format = paramsURL = '';
+
+
+// Events
 
 controls.addEventListener('click', (e)=>{
-    const podsibleOption = ['crop', 'flip', 'set']
-    if(podsibleOption.includes(e.target.getAttribute('id'))){
-        params(e.target.getAttribute('id'), e.target.value);
-    }
+    eventSelector(e.target, e.target.value, false);
+});
+
+controls.addEventListener('change', (e)=>{
+    // console.log(e.target.getAttribute('id'));
+    eventSelector(e.target, e.target.value, false);
 });
 
 controls.addEventListener('input', (e)=>{
-    params(e.target.getAttribute('id'), e.target.value);
+    eventSelector(e.target, e.target.value, true);
 });
 
 
-const params = (target, value)=>{
-    switch (target) {
-        case 'set':
+const eventSelector = (target, value, updateInputs)=>{
+
+    switch(target.getAttribute('id')){
+        case 'format':
+            format = (value === 'auto')? '': `/${value}`;
         break;
         case 'quality':
-            spans[0].innerHTML = value+'%';
+            quality = (value === '100')? '': `/${value}p`;
+            if(updateInputs){
+                inputValues[0].innerHTML = `${value}%`;
+            }
         break;
         case 'zoom':
-            width = ( originalWidth * value).toFixed(2),
-            hieght = ( originalHeight * value).toFixed(2);
-            img.style.width = `${width}px`;
-            img.style.height = `${hieght}px`;
-            // paramResize();
-            if(width >=  imgContainer.getBoundingClientRect().width && hieght >=  imgContainer.getBoundingClientRect().height){
-                imgContainer.style.justifyContent = 'flex-start';
-                imgContainer.style.alignItems = 'flex-start';
-            } else{
-                imgContainer.style.justifyContent = 'center';
-                imgContainer.style.alignItems = 'center';
-            }
-            spans[1].innerHTML = (value*100).toFixed(0)+'%';
+            width   = (originalW * value).toFixed(0);
+            height  = (originalH * value).toFixed(0);
 
-            // console.log(width, imgContainer.getBoundingClientRect().width);
+            resize = (value === '100')? '': `/resize_w/${width}`;
+
+            // TODO
+            // MOVE THIS TO IMG.ONLOAD();
+            if(width >= containerW && height >= containerH){
+                imgContainer.style.justifyContent = 'flex-start';
+                imgContainer.style.alignItems     = 'flex-start';
+            } else {
+                imgContainer.style.justifyContent = 'center';
+                imgContainer.style.alignItems     = 'center';
+            }
+
+            if(updateInputs){
+                inputValues[1].innerHTML = `${(value * 100).toFixed(0)}%`;
+            }
+
+        break;
+        case 'set':
+            focalPoints.classList.toggle('show');
+
+            if (focalPoints.classList.contains('show')){
+                img.addEventListener('click', setFocalPoint);
+                undos[0].classList.add('undo-active');
+            } else{
+                resetFocalPoint();
+            }
         break;
         case 'crop-x':
-            cropX = value;
+            value = + value;
+            cropX = (value >= imgWidth)? imgWidth: value;
+
+            // Active the undo
+            undos[1].classList.add('undo-active');
         break;
         case 'crop-y':
-            cropY = value;
+            value = + value;
+            cropY = (value >= imgHeight)? imgHeight: value;
+
+            // Active the undo
+            undos[1].classList.add('undo-active');
         break;
         case 'crop':
-            paramCrop();
-        break;
-        case 'width':
-            width = value;
-            originalWidth = value;
-            paramResize();
-        break;
-        case 'height':
-            originalHeight = value;
+            paramsCrop()
+            resetFocalPoint();
         break;
         case 'rotate':
-            paramRotate(value);
+            paramsRotate(value);
+
+            // Active the undo
+            undos[2].classList.add('undo-active');
         break;
         case 'flip':
-            paramflip();
-        break;
-        case 'hue':
-            spans[3].innerHTML = value+'%';
-            hue = scale(value, -100, 100, -1, 1).toFixed(2);
-            paramHSB(hue, saturation, brightness);         
-        break;
-        case 'saturation':
-            spans[4].innerHTML = value+'%';
-            saturation = scale(value, -100, 100, -1, 1).toFixed(2);
-            paramHSB(hue, saturation, brightness);                     
+            paramsFlip();
         break;
         case 'brightness':
-            spans[2].innerHTML = value+'%';
+            if(updateInputs){
+                inputValues[2].innerHTML = `${value}%`;
+            }
+
             brightness = scale(value, -100, 100, -1, 1).toFixed(2);
-            paramHSB(hue, saturation, brightness);         
+            paramsHSB(hue, saturation, brightness);
+        break;
+        case 'hue':
+            if(updateInputs){
+                inputValues[3].innerHTML = `${value}%`;
+            }
+
+            hue = scale(value, -100, 100, -1, 1).toFixed(2);
+            paramsHSB(hue, saturation, brightness);
+        break;
+        case 'saturation':
+            if(updateInputs){
+                inputValues[4].innerHTML = `${value}%`;
+            }
+
+            saturation = scale(value, -100, 100, -1, 1).toFixed(2);
+            paramsHSB(hue, saturation, brightness);
+        break;
+        case 'undo-set':
+            if(undos[0].classList.contains('undo-active')){
+                resetFocalPoint();
+                // focalPoints.classList.add('show');
+                // undos[0].classList.remove('undo-active');
+            }
+        break;
+        case 'undo-crop':
+            if(undos[1].classList.contains('undo-active')){
+                cropInputX.value = 250;
+                cropInputY.value = 250;
+                paramsCrop(true);
+
+                undos[1].classList.remove('undo-active')
+            }
+        break;
+        case 'undo-rotate':
+            if(undos[2].classList.contains('undo-active')){
+                paramsRotate(0);
+                rotateInput.value = 0;
+                undos[2].classList.remove('undo-active');
+            }
         break;
     }
-    buildURL();
-    changeInput(urlParams);
-}
 
-const buildURL = ()=>{
-
-    filters = filters.substring(0, filters.length - 1);
-
-    urlParams = `${HBS}${rotate}${crop}${flip}`;
-
-    if(urlParams === '/filter'){
-        urlParams = '';
-    }
-
-    console.log(url+urlParams);
-    img.setAttribute('src', `${url}${urlParams}`);
-    filters = '';
-}
-
-const changeInput = (params)=>{
-
-    paramSpan.innerHTML = (width > 0)? `/resize_w/${Math.round(width)}${params}`:params;
-}
-
-const paramHSB = (h,s,b)=>{ 
-    if(h == 0 && s == 0 && b == 0){
-        HBS = '';
-    } else{
-        HBS = `/hsb_h/${h}/hsb_s/${s}/hsb_b/${b}`;
+    if(!updateInputs){
+        urlBuilder();
+        setNewUrl(paramsURL);
+        setParamsInput(paramsURL);
     }
 }
 
-const paramflip = ()=>{
+const urlBuilder = ()=>{
+    paramsURL = `${resize}${HBS}${crop}${rotate}${flip}${quality}${format}`;
+}
+
+const setNewUrl = (params)=>{
+    img.setAttribute('src', `${baseURL}${params}`)
+}
+
+const setParamsInput = (params)=>{
+    paramsContainer.innerHTML = params;
+}
+
+const paramsHSB = (h,s,b)=>{
+    HBS = (h == 0 && s == 0 && b ==0)
+            ? ''
+            : `/hsb_h/${h}/hsb_s/${s}/hsb_b/${b}`;
+}
+
+const paramsCrop = (undo = false)=>{
+    crop = (undo)? '': `/crop_w/${cropX}/crop_h/${cropY}/fp/${focalX},${focalY}`;
+}
+
+const paramsRotate = (value)=>{
+    rotate = (value == 0)? '': `/rotate_a/${value}`;
+}
+
+const paramsFlip = ()=>{
     flip = (flip.length > 0)? '': '/flip_flip/1';
 }
 
-const paramRotate = (value)=>{
-    if(value == 0){
-        rotate = '';
-    } else {
-        rotate = `/rotate_a/${value}`;
-    }
+
+// IMG EVENT FOCAL POINT
+
+const setFocalPoint = (e)=>{
+
+    const imgLeft = img.getBoundingClientRect().left,
+    imgTop        = img.getBoundingClientRect().top,
+    imgWidth      = img.getBoundingClientRect().width,
+    imgHeight     = img.getBoundingClientRect().height,
+    x             = e.clientX - imgLeft,
+    y             = e.clientY - imgTop;
+
+    // - 5 -> Because the width and height of the point is '10px', half of 10 is '5'
+    focalPoints.style.left = `${x - 5 }px`;
+    focalPoints.style.top  = `${y - 5}px`;
+
+    focalX = scale(x, 0, imgWidth, 0, 1).toFixed(2);
+    focalY = scale(y, 0, imgHeight, 0, 1).toFixed(2);
+
+    setCoordinate(Math.round(focalX * 100), Math.round(focalY * 100));
 }
 
-const paramResize = ()=>{
-    resize = `/resize_w/${Math.round(width)}`;
-}
-
-const paramCrop = () => {
-    crop = `/crop_w/${cropX}/crop_h/${cropY}/fp/${focalX},${focalY}` 
-}
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
+// MAP SCALE
+const scale = (num, in_min, in_max, out_min, out_max)=>{
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-const setFocalPoint = (e)=>{
-    console.log(img.getBoundingClientRect(),e.clientX, point.getBoundingClientRect());
-    point.style.left = `${e.clientX - img.getBoundingClientRect().left - 4}px`;
-    point.style.top = `${e.clientY - img.getBoundingClientRect().top - 4}px`;
-    x = e.clientX - img.getBoundingClientRect().left;
-    y = e.clientY - img.getBoundingClientRect().top;
-    focalX = scale(x, 0, img.getBoundingClientRect().width, 0, 1).toFixed(2);
-    focalY = scale(y, 0, img.getBoundingClientRect().height, 0, 1).toFixed(2);
-    console.log(focalX);
-    coordinates[0].innerHTML = `${Math.round(focalX * 100)} <small>X</small>`;
-    coordinates[1].innerHTML = `${Math.round(focalY * 100)} <small>Y</small>`;
+
+// RESET FOCAL POITN
+const resetFocalPoint = ()=>{
+    focalPoints.style.left = '50%';
+    focalPoints.style.top  = '50%';
+    focalX = 0.50;
+    focalX = 0.50;
+    setCoordinate(50,50);
+    img.removeEventListener('click', setFocalPoint);
+    focalPoints.classList.remove('show');
+    undos[0].classList.remove('undo-active');
 }
 
-img.addEventListener('click',(event)=>{
-    if(point.classList.contains('show')){
-        setFocalPoint(event);
-    }
-});
-
-
-
-// Copiar URL
-
+// Copy url
 clip.addEventListener('click', ()=>{
-    let aux = document.createElement("input");
-    aux.setAttribute("value", url+urlParams);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
+
+    let clipInput = document.createElement('input');
+    
+    clipInput.setAttribute('value', baseURL+paramsURL);
+    document.body.appendChild(clipInput);
+    clipInput.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(clipInput);
+
     alert.classList.remove('d-none');
+
     setTimeout(()=>{
         alert.classList.add('d-none');
     }, 2500);
+
 });
 
-
-undos[0].addEventListener("click", ()=>{
-    point.style.left = '50%';
-    point.style.top = '50%';
-    coordinates[0].innerHTML = `50 <small>X</small>`;
-    coordinates[1].innerHTML = `50 <small>Y</small>`;
-});
-
-undos[1].addEventListener("click", ()=>{
-
-    cropX = 250;
-    cropY = 250;
-    changeInput(urlParams);
-});
-
-
-undos[2].addEventListener("click", ()=>{
-    console.log('hola');
-    flip = '/flip_flip/1';
-    rotate = '';
-    paramRotate(0);
-    rotateInput.value = 0;
-    paramflip();
-    buildURL();
-    changeInput(urlParams);
-});
+// Set Coordinates
+const setCoordinate = (x,y)=>{
+    coordinates[0].innerHTML = `${x}<small>X</small>`;
+    coordinates[1].innerHTML = `${y}<small>Y</small>`;
+}
